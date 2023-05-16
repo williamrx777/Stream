2d006fc2c102270f3e64a015e185d7a67050c86c
will <williamr723@gmail.com>
2023-05-16 01:06:01 -0300

    templates filmes animes e series views urls e a home

---
+++ home/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('filmes/', views.filmes, name='filmes'),
+    path('series/', views.series, name='series'),
+    path('animes/', views.animes, name='animes')
+]
---
+++ home/views.py
@@ -0,0 +1,23 @@
+from django.shortcuts import render
+from stream.models import Stream
+# Create your views here.
+
+def home(request):
+    return render(request,'home.html')
+
+def filmes(request):
+    if request.method == "GET":
+        nome = request.GET.get('nome')
+        filmes = Stream.objects.all()
+        if nome:
+            filmes = Stream.objects.filter(nome__icontains=nome)
+        else:
+            nome
+        return render(request,'filmes.html',{'filmes':filmes,'nome':nome})
+
+def series(request):
+    return render(request,'series.html')
+
+
+def animes(request):
+    return render(request, 'animes.html')
